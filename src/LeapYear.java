90a5c0187216ad9305d9ef6ad8a497512014c22d
hyeriByeon <bhr139313@gmail.com>
2021-01-04 16:29:45 +0900

    baekjoon 2753

--- src/LeapYear.java
+++ src/LeapYear.java
@@ -14,12 +14,7 @@ import java.util.Scanner;
 public class LeapYear {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-
         int year = scan.nextInt();
-
-        if(year % 400 == 0 || (year % 4 == 0 && year % 100 != 0))
-            System.out.println(1);
-        else
-            System.out.println(0);
+        System.out.println(year % 400 == 0 || (year % 4 == 0 && year % 100 != 0) ? 1 : 0);
     }
 }
