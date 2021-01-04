54062e983b351b61fd292a5ebf81ac7625b57a21
hyeriByeon <bhr139313@gmail.com>
2021-01-04 12:23:54 +0900

    baekjoon 2941

---
+++ src/DrawCat.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+/*
+백준 2941
+고양이 출력
+문제 : 아래 예제와 같이 고양이를 출력하시오.
+\    /\
+ )  ( ')
+(  /  )
+ \(__)|
+ */
+public class DrawCat {
+    public static void main(String[] args) {
+        System.out.println("\\    /\\");
+        System.out.println(" )  ( ')");
+        System.out.println("(  /  )");
+        System.out.println(" \\(__)|");
+    }
+}
