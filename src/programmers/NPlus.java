c7352c7f1d12e5a5f99eee01411ee8793e3bc152
eroul-ri <bhr139313@gmail.com>
2022-11-18 23:22:01 +0900

    programmers lv1 자리수더하기

---
+++ src/programmers/NPlus.java
@@ -0,0 +1,15 @@
+package programmers;
+
+public class NPlus {
+
+    public int solution(int n) {
+        int answer = 0;
+
+        while (n > 0) {
+            answer += n % 10;
+            n = n / 10;
+        }
+        return answer;
+    }
+
+}
